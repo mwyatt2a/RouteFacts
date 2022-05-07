ff9b0bc081dbe751f0418e19c587ebd9fee4d812
mwyatt2a <89659219+mwyatt2a@users.noreply.github.com>
2022-05-07 13:33:11 -0500

    Add files via upload

--- RouteFacts/app/src/main/java/com/csci3397/linhmatt/routefacts/Database.java
+++ RouteFacts/app/src/main/java/com/csci3397/linhmatt/routefacts/Database.java
@@ -14,16 +14,18 @@ public class Database extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create Table history(city Text, state Text, date Integer)");
+        db.execSQL("create Table settings(settingOn Text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop Table if exists history");
+        db.execSQL("drop Table if exists settings");
     }
 
-    public void updateDB(String city, String state, Integer date) {
+    public void updateHistory(String city, String state, Integer date) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = getDB();
+        Cursor cursor = getHistory();
         cursor.moveToFirst();
         boolean unique = true;
         for (int i = 0; i < cursor.getCount(); i++) {
@@ -43,7 +45,36 @@ public class Database extends SQLiteOpenHelper {
         }
     }
 
-    public Cursor getDB() {
+    public void updateSettings(String setting) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = getSettings();
+        cursor.moveToFirst();
+        boolean notExists = true;
+        for (int i = 0; i < cursor.getCount(); i++) {
+            if (cursor.getString(0).equals(setting)) {
+                notExists = false;
+            }
+            else {
+                cursor.moveToNext();
+            }
+        }
+        if (notExists) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("settingOn", setting);
+            db.insert("settings", null, contentValues);
+        }
+        else {
+            db.delete("settings","settingOn = ?", new String[]{setting});
+        }
+    }
+
+    public Cursor getSettings() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query("settings", null, null, null, null, null, null, null);
+        return cursor;
+    }
+
+    public Cursor getHistory() {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query("history", null, null, null, null, null, "date", null);
         return cursor;
